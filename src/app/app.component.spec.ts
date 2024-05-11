4d3d331cbdf4900c20faa4106279b3b0194dddac
Sundarm <sundarm.soni097@gmail.com>
2024-05-11 15:00:14 +0530

    feature/Initial setup done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+  beforeEach(waitForAsync(() => {
+     TestBed.configureTestingModule({
+      imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome secureworks-assignment'
-    );
-  });
-
-  it(`should have as title 'secureworks-assignment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('secureworks-assignment');
-  });
+  }));
 });
